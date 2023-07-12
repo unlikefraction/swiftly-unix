6d89c3b34eee462277968ed3ce49779f98640981
Shubham Gupta <shubhastro2@gmail.com>
2023-07-12 19:15:53 +0530

    0.1.27

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='swiftly-unix',
-    version='0.1.25',
+    version='0.1.27',
     packages=find_packages(),
     scripts=['scripts/swiftly'],
 )
